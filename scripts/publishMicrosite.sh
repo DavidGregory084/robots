f8a728c9911ef6ed85c8218037d2bbe7baf8e32f
David Gregory <davidgregory084@gmail.com>
2017-07-14 15:55:47 +0100

    Workaround for weird SBT behaviour

--- scripts/publishMicrosite.sh
+++ scripts/publishMicrosite.sh
@@ -5,4 +5,4 @@ git config --global user.email "davidgregory084@gmail.com"
 git config --global user.name "David Gregory"
 git config --global push.default simple
 
-sbt docs/publishMicrosite
+sbt "project docs" publishMicrosite
